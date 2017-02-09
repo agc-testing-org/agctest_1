2a3d3acc7a2976c72b0fb257526e7fe33dff7b92
Adam <adam@nativecurrent.com>
2017-02-08 23:54:27 -0800

    icons

--- integrations-client/app/routes/develop/project/sprint.js
+++ integrations-client/app/routes/develop/project/sprint.js
@@ -3,15 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     store: Ember.inject.service(),
     model: function(params){
-
-        this.store.adapterFor('sprint').set('namespace', 'projects/' + params.name.split("-")[0] );
-        this.store.adapterFor('event').set('namespace', 'projects/' + params.name.split("-")[0] );
+        console.log(this.paramsFor("develop.project").org);
+        console.log(this.modelFor("develop.project"));
 
         return Ember.RSVP.hash({
-            project: this.store.findRecord('project', params.name.split("-")[0]),
-            events: this.store.findAll('event'),
-            sprints: this.store.findAll('sprint'),
-            states: this.store.findAll('state')
+            sprint: this.store.peekRecord('sprint', params.id)
         });
     }
 });
