9919d457922a17a9180423c3a5181be2acd0fcf0
Adam <adam@nativecurrent.com>
2017-02-12 12:28:53 -0800

    updating fixtures for instruction

--- db/schema.rb
+++ db/schema.rb
@@ -137,11 +137,12 @@ ActiveRecord::Schema.define(version: 20170211015731) do
   add_index "sprints", ["user_id"], name: "index_sprints_on_user_id", using: :btree
 
   create_table "states", force: :cascade do |t|
-    t.string   "name",        limit: 255, null: false
-    t.string   "fa_icon",     limit: 255, null: false
-    t.string   "description", limit: 255, null: false
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.string   "name",        limit: 255,   null: false
+    t.string   "fa_icon",     limit: 255,   null: false
+    t.string   "description", limit: 255,   null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.text     "instruction", limit: 65535, null: false
   end
 
   create_table "user_roles", force: :cascade do |t|
