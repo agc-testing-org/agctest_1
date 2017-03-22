2b69f6e539ee15a348220c3b197ad7c8ed9d034c
Adam <adam@nativecurrent.com>
2017-03-21 22:24:52 -0700

    home shows profile

--- integrations-client/app/routes/home.js
+++ integrations-client/app/routes/home.js
@@ -3,15 +3,25 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 
 export default Ember.Route.extend(AuthenticatedRouteMixin,{
     store: Ember.inject.service(),
+    sessionAccount: Ember.inject.service('session-account'),
     model: function(params) {
         //var post_id = this.modelFor('post.show').get('id');
         //        this.store.adapterFor('sprint').set('namespace', 'projects/' + 1 );
+//        console.log(this.get('session-account'));
+        var username = this.get('sessionAccount').account.id;
 
         return Ember.RSVP.hash({
+            states: this.store.findAll('state'),
             repositories: this.store.findAll('repository'),
-            //            sprints: this.store.findAll('sprint')
-
-
+            comments: this.store.queryRecord('aggregate-comment', {
+                user_id: username
+            }),
+            votes: this.store.queryRecord('aggregate-vote', {
+                user_id: username
+            }),
+            contributors: this.store.queryRecord('aggregate-contributor', {
+                user_id: username
+            })
         });
     }
 });
