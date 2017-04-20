91419b50fb7d2e34d3e204d1f130226a16b2a787
Adam <adam@nativecurrent.com>
2017-04-20 07:48:57 -0700

    preparing to show sprint states based on contribution acceptance

--- integrations-client/app/routes/develop/project/sprint/state.js
+++ integrations-client/app/routes/develop/project/sprint/state.js
@@ -9,8 +9,15 @@ export default Ember.Route.extend({
         this.store.adapterFor('skillset').set('namespace', 'sprints/' + params.id );
         var all_states = this.modelFor("develop.project.sprint").sprint.get("sprint_states").toArray();
         var last_state = {};
+        var last_contributor_state = {};
         if(all_states.length > 0){
             last_state = all_states[all_states.length - 1];
+
+            for(var i = 0; i < all_states.length; i++){
+                if(all_states[i].id === params.state_id){
+                    last_contributor_state = all_states[i - 1]; // state before current 
+                }
+            }
         }
 
         return Ember.RSVP.hash({
@@ -28,7 +35,9 @@ export default Ember.Route.extend({
 
             selected_state: this.store.peekRecord('sprint-state', params.state_id),
 
-            last_state: last_state
+            last_state: last_state,
+
+            last_contributor_state: last_contributor_state
 
         });
     }
