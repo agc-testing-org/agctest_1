06dacfdeaf6dbe3d0d030da38587026bf485fe20
masharoundabout <masharoundabout@gmail.com>
2017-05-25 01:14:54 +0300

    Define a route for UI notifications/contacts info

--- integrations-client/app/router.js
+++ integrations-client/app/router.js
@@ -7,22 +7,24 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-    this.route('index', {path: '/'});
-    this.route('home');
-    this.route('forgot');
-    this.route('register');
-    this.route('token',{ path: '/token/:id' });
-    this.route('forgot');
-    this.route('develop', {path: '/develop'}, function() {
-        this.route('index', {path: '/'});
-        this.route('project', {path: '/:org/:name'}, function() {
-            this.route('state', {path: '/state/:id'});
-            this.route('sprint', {path: '/sprint/:id'}, function() {
-                this.route('state', { path: '/state/:state_id' });
-            }); 
-        });
-    });
-    this.route('profile',{ path: '/:username'});
+  this.route('index', {path: '/'});
+  this.route('home');
+  this.route('forgot');
+  this.route('register');
+  this.route('token',{ path: '/token/:id' });
+  this.route('forgot');
+  this.route('develop', {path: '/develop'}, function() {
+      this.route('index', {path: '/'});
+      this.route('project', {path: '/:org/:name'}, function() {
+          this.route('state', {path: '/state/:id'});
+          this.route('sprint', {path: '/sprint/:id'}, function() {
+              this.route('state', { path: '/state/:state_id' });
+          }); 
+      });
+  });
+  this.route('profile',{ path: '/:username'});
+  this.route('notifications');
+  this.route('connections');
 });
 
 export default Router;
