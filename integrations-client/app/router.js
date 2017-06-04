daffc9a030747e51e8268be97eb233c4483db079
masharoundabout <masharoundabout@gmail.com>
2017-06-04 15:07:25 +0300

    resolving merge conflict v2

--- integrations-client/app/router.js
+++ integrations-client/app/router.js
@@ -1,3 +1,5 @@
+export default Router;
+
 import Ember from 'ember';
 import config from './config/environment';
 
@@ -7,26 +9,30 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('index', {path: '/'});
-  this.route('home');
-  this.route('forgot');
-  this.route('register');
-  this.route('token',{ path: '/token/:id' });
-  this.route('forgot');
-  this.route('develop', {path: '/develop'}, function() {
-      this.route('index', {path: '/'});
-      this.route('project', {path: '/:org/:name'}, function() {
-          this.route('state', {path: '/state/:id'});
-          this.route('sprint', {path: '/sprint/:id'}, function() {
-              this.route('state', { path: '/state/:state_id' });
-          }); 
-      });
-  });
-  this.route('profile',{ path: '/:username'});
-  this.route('notifications');
-  this.route('connections');
-  this.route('requests');
-  this.route('home2');
+    this.route('index', {path: '/'});
+    this.route('home');
+    this.route('forgot');
+    this.route('register');
+    this.route('login'); 
+    this.route('token',{ path: '/token/:id' });
+    this.route('invitation', {path: '/invitation/:id'});
+    this.route('registered-invitation', {path: '/registered-invitation/:id'});
+    this.route('forgot');
+    this.route('develop', {path: '/develop'}, function() {
+        this.route('index', {path: '/'});
+        this.route('project', {path: '/:org/:name'}, function() {
+            this.route('state', {path: '/state/:id'});
+            this.route('sprint', {path: '/sprint/:id'}, function() {
+                this.route('state', { path: '/state/:state_id' });
+            }); 
+        });
+    });
+    this.route('team', {path: '/team/:id'});
+    this.route('profile',{ path: '/:username'});
+    this.route('notifications');
+    this.route('connections');
+    this.route('requests');
+    this.route('home2');
 });
 
 export default Router;
