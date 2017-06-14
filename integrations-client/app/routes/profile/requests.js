e004539c0562c629ff1177643cef32604b98c0c0
mashawired7 <masha@wired7.com>
2017-06-14 17:09:49 +0300

    POST contact request v2

--- integrations-client/app/routes/profile/requests.js
+++ integrations-client/app/routes/profile/requests.js
@@ -2,7 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model: function (params, transition) {
-    	var id = this.paramsFor('profile').id;
-    	console.log(id);
+        var id = this.paramsFor('profile').id;
+        console.log(id);
+        var store = this.get('store');
+        store.adapterFor('requests').set('namespace', 'account');
+        var requestPost = store.createRecord('request', {
+            contact_id: id
+        }).save()
     }
 });
