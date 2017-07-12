ba281462facb9652b20ef31d82607be98a0ee96e
Маша <masha@wired7.com>
2017-07-12 22:59:05 +0300

    requests and contacts order by desc

--- integrations-client/app/models/connection.js
+++ integrations-client/app/models/connection.js
@@ -8,5 +8,7 @@ export default DS.Model.extend({
     created_at: attr('date'),
     updated_at: attr('date'),
     contact_id: attr('number'),
-    user_id: attr('number')
+    user_id: attr('number'),
+    read: DS.attr('boolean'),
+	confirmed: DS.attr('number')
 });
