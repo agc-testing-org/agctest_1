4e0b7a017ff0acbfc58986d034ae67297f84e47d
adam <adam@wired7.com>
2017-07-12 11:29:44 -0700

    updates to aggregates/onboarding

--- integrations-client/app/components/profile-overview.js
+++ integrations-client/app/components/profile-overview.js
@@ -4,6 +4,10 @@ export default Ember.Component.extend({
     session: Ember.inject.service('session'),
     store: Ember.inject.service(),
     sessionAccount: Ember.inject.service('session-account'),
+    didRender() {
+        this._super(...arguments);
+        this.$('[data-toggle="tooltip"]').tooltip('show');
+    },
     actions: {
         refresh(){
             this.sendAction("refresh");
