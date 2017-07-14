22685f75deb8e7795d9efef8b86127c8f6fef30a
Маша <masha@wired7.com>
2017-07-14 20:06:47 +0300

    UI for team notifications

---
+++ integrations-client/app/routes/team/select/notifications.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+    queryParams: {
+        page: {
+            refreshModel: true
+        }
+    },
+    model: function (params) {
+        var id = this.paramsFor("team.select").id
+        this.store.adapterFor('notification').set('namespace', 'teams/'+id);
+        var notifications = this.store.query('notification',params);
+        this.store.adapterFor('notification').set('namespace', '');
+
+        return Ember.RSVP.hash({
+            notifications: notifications,
+            params: params
+        });
+    }
+});
+
--- lib/api/integrations.rb
+++ lib/api/integrations.rb
@@ -1243,7 +1243,7 @@ class Integrations < Sinatra::Base
     get "/teams", &teams_get
     get "/teams/:id", allows: [:id], needs: [:id], &teams_get_by_id
     get "/team-invites", &team_invites_get
-    get "/teams/:id/notifications", allows: [:id], needs: [:id], &teams_notifications_get
+    get "/teams/:id/notifications", allows: [:id, :page], needs: [:id], &teams_notifications_get
 
     post "/user-teams/token", &user_teams_patch
     post "/user-teams", &user_teams_post
