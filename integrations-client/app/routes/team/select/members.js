aa1ea405b3e0b5e138bef17618d3c5917dcf88ab
adam <adam@wired7.com>
2017-07-19 00:30:39 -0700

    aggregates on talent only

--- integrations-client/app/routes/team/select/members.js
+++ integrations-client/app/routes/team/select/members.js
@@ -11,40 +11,12 @@ export default Ember.Route.extend({
     model: function(params) {
 
         var memberSeat = this.store.peekAll("seat").findBy("name","member");
-        var memberSeatId = memberSeat.get("id");
-
-        this.store.adapterFor('team-comment').set('namespace', 'user-teams/' + this.paramsFor("team.select").id);
-        var user_teams_comments = this.store.query('team-comment', {
-            seat_id: memberSeatId
-        });
-        var user_teams_votes = this.store.query('team-vote', {
-            seat_id: memberSeatId
-        });
-        var user_teams_contributors = this.store.query('team-contributor', {
-            seat_id: memberSeatId
-        });
-        var user_teams_comments_received = this.store.query('team-comments-received', {
-            seat_id: memberSeatId
-        });
-        var user_teams_votes_received = this.store.query('team-votes-received', {
-            seat_id: memberSeatId
-        });
-        var user_teams_contributors_received = this.store.query('team-contributors-received', {
-            seat_id: memberSeatId
-        }); 
-        this.store.adapterFor('team-comment').set('namespace', '');
 
         return Ember.RSVP.hash({
             team: this.modelFor("team.select").team,
-            user_teams_comments: user_teams_comments,
-            user_teams_comments_received: user_teams_comments_received,
-            user_teams_votes: user_teams_votes,
-            user_teams_votes_received: user_teams_votes_received,
-            user_teams_contributors: user_teams_contributors,
-            user_teams_contributors_received: user_teams_contributors_received,
             user_teams: this.store.query('user-team', {
                 team_id: this.paramsFor("team.select").id,
-                seat_id: memberSeatId
+                seat_id: memberSeat.get("id")
             }),
             default_seat: memberSeat
         });
--- integrations-client/app/routes/team/select/talent.js
+++ integrations-client/app/routes/team/select/talent.js
@@ -17,7 +17,35 @@ export default Ember.Route.extend({
             defaultSeatId = defaultSeat.get("id")
         }
 
+        this.store.adapterFor('team-comment').set('namespace', 'user-teams/' + this.paramsFor("team.select").id);
+        var user_teams_comments = this.store.query('team-comment', {
+            seat_id: defaultSeatId
+        });
+        var user_teams_votes = this.store.query('team-vote', {
+            seat_id: defaultSeatId
+        });
+        var user_teams_contributors = this.store.query('team-contributor', {
+            seat_id: defaultSeatId
+        });
+        var user_teams_comments_received = this.store.query('team-comments-received', {
+            seat_id: defaultSeatId
+        });
+        var user_teams_votes_received = this.store.query('team-votes-received', {
+            seat_id: defaultSeatId
+        });
+        var user_teams_contributors_received = this.store.query('team-contributors-received', {
+            seat_id: defaultSeatId
+        });
+        this.store.adapterFor('team-comment').set('namespace', '');
+
+
         return Ember.RSVP.hash({
+            user_teams_comments: user_teams_comments,
+            user_teams_comments_received: user_teams_comments_received,
+            user_teams_votes: user_teams_votes,
+            user_teams_votes_received: user_teams_votes_received,
+            user_teams_contributors: user_teams_contributors,
+            user_teams_contributors_received: user_teams_contributors_received,
             team: this.modelFor("team.select").team,
             user_teams: this.store.query('user-team', {
                 team_id: this.paramsFor("team.select").id,
