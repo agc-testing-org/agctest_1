ce0106c82ef07c2ae88d2eece6978f48582e057f
Маша <masha@wired7.com>
2017-07-31 16:37:09 +0300

    db migrate for comment upvote

--- lib/models/comment.rb
+++ lib/models/comment.rb
@@ -10,4 +10,5 @@ class Comment < ActiveRecord::Base
     belongs_to :contributor
     belongs_to :sprint_state
     belongs_to :user
+    has_many :votes
 end
--- lib/models/vote.rb
+++ lib/models/vote.rb
@@ -9,5 +9,6 @@ class Vote < ActiveRecord::Base
     belongs_to :contributor
     belongs_to :sprint_state
     belongs_to :user
+    belongs_to :comment
     validates_uniqueness_of :user, scope: :sprint_state
 end
