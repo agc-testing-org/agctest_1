9db9216ad2f2672d591967404ea1bf59392ec5ce
adam <adam@wired7.com>
2017-08-09 19:34:24 -0700

    updating language

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170807001022) do
+ActiveRecord::Schema.define(version: 20170810015145) do
 
   create_table "comments", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "user_id", null: false
