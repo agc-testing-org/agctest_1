9019750a903d6eab07c30efb0679735741335883
adam <adam@wired7.com>
2017-08-18 17:57:30 -0700

    removing dependence on owner

--- integrations-client/app/components/profile-content-stats.js
+++ integrations-client/app/components/profile-content-stats.js
@@ -1,10 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    session: Ember.inject.service('session'),
-    store: Ember.inject.service(),
-    sessionAccount: Ember.inject.service('session-account'),
     activeRoles: Ember.computed.filterBy("roles","active",true),
     recruiter: Ember.computed.filterBy("activeRoles","name","recruiting"),
     manager: Ember.computed.filterBy("activeRoles","name","management"),
+    didRender() {
+        this._super(...arguments);
+    },
+    actions: {
+        refresh(){
+            this.sendAction("refresh");
+        }
+    }
 });
