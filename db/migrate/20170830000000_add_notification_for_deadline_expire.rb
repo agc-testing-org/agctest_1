de3f349117e4cc167fdfea3a11bb392d59891636
mmalkova <masha@wired7.com>
2017-08-31 15:53:08 +0300

    rspec for expires

--- db/migrate/20170830000000_add_notification_for_deadline_expire.rb
+++
@@ -1,5 +0,0 @@
-class AddNotificationForDeadlineExpire < ActiveRecord::Migration[5.1]
-  def change
-      Notification.create("name": "deadline expire", "description": "the deadline for the sprint expire")
-  end
-end
