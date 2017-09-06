3c1b37bb1d7e5d69bc504e9dc703ec0942b9942f
adam <adam@wired7.com>
2017-09-05 21:11:01 -0700

    introducing top nav

--- integrations-client/app/components/application-nav.js
+++ integrations-client/app/components/application-nav.js
@@ -1,6 +1,27 @@
 import Ember from 'ember';
+import config from 'integrations-client/config/environment';
 
-export default Ember.Component.extend({
-    session: Ember.inject.service('session'),
-    sessionAccount: Ember.inject.service('session-account')
+const { inject: { service }, Component } = Ember;
+
+export default Component.extend({
+    splash: config.splash,
+    session: service('session'),
+    sessionAccount: Ember.inject.service('session-account'),
+    showTeams: false,
+    activeRoles: Ember.computed.filterBy("roles","active",true),
+    recruiter: Ember.computed.filterBy("activeRoles","name","recruiting"),
+    manager: Ember.computed.filterBy("activeRoles","name","management"),
+    didRender() {
+        this._super(...arguments);
+    },
+    actions: {
+        displayTeams(){
+            if(this.get("showTeams")){
+                this.set("showTeams",false);
+            }
+            else {
+                this.set("showTeams",true);
+            }
+        }
+    }
 });
