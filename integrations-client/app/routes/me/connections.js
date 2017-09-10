291bc381970ea1d244a5908858cabc1ec54a3ba0
adam <adam@wired7.com>
2017-09-09 20:48:18 -0700

    removing requests from contacts

--- integrations-client/app/routes/me/connections.js
+++ integrations-client/app/routes/me/connections.js
@@ -2,16 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model: function () {
-    	this.store.adapterFor('connection').set('namespace', 'users/me');
-    	var connections = this.store.findAll('connection');
-        this.store.adapterFor('connection').set('namespace', '');
-        this.store.unloadAll('request'); 
-        this.store.adapterFor('request').set('namespace', 'users/me');
-        var requests = this.store.findAll('request');
+        this.store.unloadAll('request');
+        this.store.adapterFor('connection').set('namespace', 'users/me');
+        var connections = this.store.findAll('connection');
         this.store.adapterFor('request').set('namespace', '');
         return Ember.RSVP.hash({
             connections: connections,
-            requests: requests,
             user: this.modelFor("me").user
         });
     }
