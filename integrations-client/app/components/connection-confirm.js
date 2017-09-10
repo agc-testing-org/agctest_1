6a34b49c732f1707b7da8c00c2ab0976a9a45dfd
adam <adam@wired7.com>
2017-09-09 20:25:36 -0700

    contact request updates on profile

--- integrations-client/app/components/connection-confirm.js
+++ integrations-client/app/components/connection-confirm.js
@@ -5,16 +5,22 @@ export default Ember.Component.extend({
     store: Ember.inject.service(),
     sessionAccount: Ember.inject.service('session-account'),
     actions: {
+        refresh(){
+            this.sendAction("refresh");
+        },  
         confirmeUserConnectionRequests(id, confirmed){
 
+            var _this = this;
             var store = this.get('store');
 
             store.adapterFor('connections').set('namespace', 'users/me');
 
             var requestConfirme = store.findRecord('connection', id).then(function (connection) {
                 connection.set('confirmed', confirmed);
+                connection.set('read', 1);
                 connection.save().then(function () {
                     store.adapterFor('connections').set('namespace', '');
+                    _this.sendAction("refresh");
                 });
             });
         }
